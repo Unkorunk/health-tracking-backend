59a0bf1a5d7b98c40ed8209e58d2f13a26d8f468
Vladislav Kalinin <unkorunk@gmail.com>
2020-05-03 20:19:18 +1000

    Add example for work with database

--- src/main/java/ru/timelimit/example/HelloWorldController.kt
+++ src/main/java/ru/timelimit/example/HelloWorldController.kt
@@ -1,6 +1,12 @@
 package ru.timelimit.example
 
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -9,8 +15,38 @@ class HelloWorldController {
         val resultStr: String
     )
 
+    data class LoginResult(
+        val status: Boolean,
+        val token: String
+    )
+
+
+    object Users : Table("epidemic_db.public.Users") {
+        val id = integer("id")
+        val username = varchar("username", 64)
+        val password = varchar("password", 64)
+        val role = integer("role")
+        val firstName = varchar("firstName", 64)
+        val lastName = varchar("lastName", 64)
+    }
+
     @RequestMapping("/example/HelloWorld")
     fun helloWorld(): HelloWorldResult {
         return HelloWorldResult("Hello World")
+    }
+
+    @RequestMapping("/account/login")
+    fun login(@RequestParam("login") login: String, @RequestParam("password") password: String)
+            : LoginResult {
+        var status = false
+
+        transaction {
+            val req = Users.select { Users.username eq login }
+            if (req.count() == 1L && req.single()[Users.password] == password) {
+                status = true
+            }
+        }
+
+        return LoginResult(status, "TODO")
     }
 }
--- src/main/java/ru/timelimit/main.kt
+++ src/main/java/ru/timelimit/main.kt
@@ -1,5 +1,6 @@
 package ru.timelimit
 
+import org.jetbrains.exposed.sql.Database
 import org.springframework.boot.runApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
@@ -7,5 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 open class HealthTrackingBackend
 
 fun main(args: Array<String>) {
+    Database.connect(
+        "jdbc:postgresql://104.248.59.99:5432/epidemic_db", driver = "org.postgresql.Driver",
+        user = "epidemic_user", password = "6QAeE2f8f4tzZ3cGSnLmpaF8"
+    )
     runApplication<HealthTrackingBackend>(*args)
 }
