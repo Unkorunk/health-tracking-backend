b831653785d2446df91556146150f7edbbf17cdb
LuRomanenkova <romanenkova.ls@students.dvfu.ru>
2020-05-04 11:12:02 +1000

    Add login into AuthController.kt

--- src/main/java/ru/timelimit/account/AuthController.kt
+++ src/main/java/ru/timelimit/account/AuthController.kt
@@ -1,16 +1,16 @@
 package ru.timelimit.account
 
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.StdOutSqlLogger
-import org.jetbrains.exposed.sql.addLogger
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import ru.timelimit.example.HelloWorldController
+import ru.timelimit.account.Users.token
+
 import java.util.*
+import kotlin.streams.asSequence
 
 data class Response(val status: Boolean, val result: String, val message: String? = null)
 
@@ -19,6 +19,15 @@ internal class AuthController {
     data class RegistrationResult(
         val status: Boolean
     )
+
+    data class LoginResult(
+        val status: Boolean,
+        val token: String,
+        val expires_in: String
+    )
+
+    private val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
     @RequestMapping("/account/registration")
     fun registration(@RequestParam("login") login: String, @RequestParam("password") password: String,
                      @RequestParam("role", defaultValue = "0") role: Int,
@@ -46,6 +55,38 @@ internal class AuthController {
             }
         }
         return RegistrationResult(status)
+    }
+
+
+
+    @RequestMapping("/account/login")
+    fun login(@RequestParam("login") login: String, @RequestParam("password") password: String): LoginResult {
+        var status = false
+
+        var randomString: String = ""
+        var dateTime: DateTime = DateTime.now()
+        transaction {
+            addLogger ( StdOutSqlLogger )
+
+            val req = Users.select { Users.username eq login }
+
+            if (req.count() == 1L && req.single()[Users.password] == password) {
+                randomString = (1..128)
+                    .map { i -> kotlin.random.Random.nextInt(0, charPool.size) }
+                    .map(charPool::get)
+                    .joinToString("")
+                dateTime = DateTime.now().plusMinutes(10)
+
+                Users.update ({ Users.username eq login }) {
+                    it[Users.token] = randomString
+                    it[Users.expires_in] = dateTime
+                }
+
+                status = true
+            }
+        }
+
+        return LoginResult(status, randomString, dateTime.toString() )
     }
 
 }
