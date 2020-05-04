cc5a8d9f25dba0efd8a40a4b3b884111f7b207c0
LuRomanenkova <romanenkova.ls@students.dvfu.ru>
2020-05-04 15:55:07 +1000

    Change field Status of Contacts table from string type to bool, change method Add in the ContactsController due to Contacts table, create Get method in the ContactsController

--- src/main/java/ru/timelimit/contact/ContactClass.kt
+++ src/main/java/ru/timelimit/contact/ContactClass.kt
@@ -1,17 +1,12 @@
 package ru.timelimit.contact
 
-import ru.timelimit.contact.Contacts
-import ru.timelimit.contact.Contacts.autoIncrement
-
 import org.jetbrains.exposed.sql.Table
 import ru.timelimit.account.Users
-import ru.timelimit.feed.Feed
-import ru.timelimit.feed.Feed.references
 
 object Contacts : Table("epidemic_db.public.contacts") {
     val id = Contacts.integer("id").autoIncrement()
     val name = Contacts.varchar("name", 64)
     val phone_number = Contacts.varchar("phone_number", 11)
-    val status = Contacts.varchar("status", 64)
-    val user_id = Feed.integer("user_id").references(Users.id)
+    val status = Contacts.bool("status")
+    val user_id = Contacts.integer("user_id").references(Users.id)
 }
--- src/main/java/ru/timelimit/contact/ContactsController.kt
+++ src/main/java/ru/timelimit/contact/ContactsController.kt
@@ -1,30 +1,28 @@
 package ru.timelimit.contact
 
-import org.hibernate.validator.internal.util.Contracts.assertTrue
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.joda.time.DateTime
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import ru.timelimit.account.Users
 
+
 import ru.timelimit.utility.AccountUtility
 
 @RestController
 class ContactsController {
-//    data class Contact(
-//            val contact_name: String,
-//            val phone_number: String,
-//            val status: String
-//    )
+    data class Contact(
+            val contact_name: String,
+            val phone_number: String,
+            val status: Boolean
+    )
 
     data class GetContactsResult(
             val status: Boolean,
-            val contacts: List<Contacts> = listOf()
+            val contacts: List<Contact> = listOf()
     )
 
     fun CheckNumberValidation(phone_number: String) : Boolean {
@@ -36,7 +34,7 @@ class ContactsController {
     @RequestMapping("contacts/add")
     fun add(@RequestParam("name") name: String,
             @RequestParam("phone_number") phone: String,
-            @RequestParam("status", defaultValue = "Null") contact_status: String,
+            @RequestParam("status", defaultValue = false.toString()) contact_status: Boolean,
             @RequestParam("token") token: String) : Map<String, Boolean> {
         val user = AccountUtility.getUserByToken(token) ?: return mapOf(Pair("status", false))
 
@@ -56,6 +54,22 @@ class ContactsController {
         }
 
         return mapOf(Pair("status", status))
+    }
+
+    @RequestMapping("contacts/get")
+    fun get(@RequestParam("token") token: String) : ContactsController.GetContactsResult {
+        val user = AccountUtility.getUserByToken(token) ?: return ContactsController.GetContactsResult(false)
+
+        val contacts = mutableListOf<ContactsController.Contact>()
+        transaction {
+            Contacts.selectAll().forEach {
+
+                contacts.add(ContactsController.Contact(it[Contacts.name],
+                            it[Contacts.phone_number], it[Contacts.status]))
+
+            }
+        }
+        return ContactsController.GetContactsResult(true, contacts.toList())
     }
 
 }
